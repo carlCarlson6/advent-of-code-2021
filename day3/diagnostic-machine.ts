55c64c7146f9a2435bb0a251794ab1d095eb0be4
Carlos Acitores Deval <carlos.acitores@colba.es>
2021-12-12 20:21:09 +0100

    wip on part 2

--- day3/diagnostic-machine.ts
+++ day3/diagnostic-machine.ts
@@ -1,32 +1,32 @@
 export class DiagnosticMarchine {
-    private gamma: string = '';
-    private epsilon: string = '';
+    private gamma = '';
+    private epsilon = '';
+    private oxygenGeneratorRate = '';
+    private CO2ScrubberRating = '';
 
-    processData(binaryData: string[]): void {
-        this.gamma = this.calculateGammaRate(binaryData);
-        this.epsilon = this.calculateEpsilonRate(this.gamma);
-    }
-
-    private calculateGammaRate(binaryData: string[]): string {        
-        return this.transpose(binaryData.map(data => data.split(''))).map(data => this.mode(data)).join('');
-    }
-
-    private calculateEpsilonRate(gammaRate: string): string {
-        return this.gamma.split('').map(g => g === '1'? '0': '1').join('');
+    processGammaEpsilonRates(binaryData: string[]): void {
+        this.gamma = this.transpose(binaryData.map(data => data.split(''))).map(data => this.mode(data)).join('');
+        this.epsilon = this.gamma.split('').map(g => g === '1'? '0': '1').join('');
     }
 
     calculatePowerConsumption(): number {
         return parseInt(this.gamma, 2) * parseInt(this.epsilon, 2);
     }
 
-    private transpose(matrix: string[][]) {
+    processOxygenCO2Rates(binaryData: string[]): void {}
+
+    calculateLifeSupportRating(): number {
+        return parseInt(this.oxygenGeneratorRate, 2) * parseInt(this.CO2ScrubberRating, 2);
+    }
+
+    private transpose(matrix: string[][]): string[][] {
         return matrix[0].map((x,i) => matrix.map(x => x[i]));
     }
 
-    private mode(arr: string[]){
+    private mode(arr: string[]): string {
         return arr.sort((a,b) =>
               arr.filter(v => v===a).length
             - arr.filter(v => v===b).length
-        ).pop();
+        ).pop()!;
     }
 }
--- day3/index.ts
+++ day3/index.ts
@@ -1,11 +1,15 @@
-import { read, readFileSync } from "fs";
+import { readFileSync } from "fs";
 import { DiagnosticMarchine } from "./diagnostic-machine";
 
 const readInput = (): string[] => readFileSync('./day3/input.txt', 'utf-8').split('\n');
 const rawData = readInput();
 
 const diagnostic = new DiagnosticMarchine();
-diagnostic.processData(rawData);
 
+diagnostic.processGammaEpsilonRates(rawData);
 const powerConsumption = diagnostic.calculatePowerConsumption();
-console.log(powerConsumption);
+console.log('powerConsumption', powerConsumption);
+
+diagnostic.processOxygenCO2Rates(rawData);
+const ligeSupportRating = diagnostic.calculateLifeSupportRating()
+console.log('ligeSupportRating', ligeSupportRating);
